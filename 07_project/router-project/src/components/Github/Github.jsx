61bbaa433074f55397073f66fbd0c0769d73406d
Rajeel Siddiqui <159783237+RajeelSiddiqui1@users.noreply.github.com>
2025-03-15 19:38:03 +0500

    make router project

---
+++ 07_project/router-project/src/components/Github/Github.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+
+    const data = useLoaderData()
+
+    // const [data, setData] = useState([])
+    
+    // useEffect(()=>{
+    //     fetch(`https://api.github.com/users/hiteshchoudhary`)
+    //     .then((res)=> res.json())
+    //     .then(data => {
+    //         setData(data)
+    //     })
+    // },[])
+
+    if (!data) {
+        return <div className="text-white text-3xl bg-gray-600 p-4">Loading...</div>;
+    }
+
+  return (
+    <>
+    <div className='text-white text-center text-3xl bg-gray-600 p-4'>
+        Followers: {data.followers}
+        <img src={data.avatar_url} alt="Github picture" width={300}/>
+    </div>
+    </>
+  )
+}
+
+export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch(`https://api.github.com/users/hiteshchoudhary`)
+     if (!response.ok){
+       console.log('error')
+     }
+     return response.json()
+}
