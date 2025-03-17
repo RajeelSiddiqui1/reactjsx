27af4047409c62c2786a194889f4845423b5af67
Rajeel Siddiqui <159783237+RajeelSiddiqui1@users.noreply.github.com>
2025-03-17 23:48:33 +0500

    make an login singup authlayout with react hook form

---
+++ 12_mega_project/blog/src/components/Singup.jsx
@@ -0,0 +1,98 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button, Input, Logo } from './index'
+import authService from '../appwrite/auth'
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { login as authLogin } from '../stores/authSlice'
+import { useState } from 'react'
+
+function Singup() {
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const { register, handleSubmit } = useForm()
+    const [error, setError] = useState('')
+
+    const singup = async (data) => {
+        setError('')
+        try {
+           const userData = await authService.createAccount(data)
+           if(userData){
+               const session = await authService.getCurrentUser()
+               if (session) dispatch(authLogin(session))
+                navigate('/')
+           }
+        } catch (error) {
+            setError(error.message)
+        }
+    }
+
+  return (
+    <div className="flex items-center justify-center">
+    <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
+    <div className="mb-2 flex justify-center">
+            <span className="inline-block w-full max-w-[100px]">
+                <Logo width="100%" />
+            </span>
+        </div>
+        <h2 className="text-center text-2xl font-bold leading-tight">Sign up to create account</h2>
+        <p className="mt-2 text-center text-base text-black/60">
+            Already have an account?&nbsp;
+            <Link
+                to="/login"
+                className="font-medium text-primary transition-all duration-200 hover:underline"
+            >
+                Sign In
+            </Link>
+        </p>
+        {error && <p className="text-red-600 text-center mt-8">{error}</p>}
+        </div>
+
+          <form onSubmit={handleSubmit(singup)}>
+            <div className='space-y-5'>
+                <Input
+                 label = 'Name'
+                 placeholder = 'Enter your name'
+                 {...register("name",{
+                        required: true,
+                        minLength: 5,
+                        maxLength: 255,
+                 })}
+                />
+
+                 <Input
+                    label = 'Email'
+                    type = 'email'
+                    placeholder = 'Enter your email'
+                    {...register("email", {
+                     required: true,
+                     validate:{
+                       matchPattern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) || "Email address must be a valid address"
+                     }
+                    })}         
+                />
+
+                <Input
+                    label = 'Password'
+                    type = 'password'
+                    placeholder = 'Enter your password'
+                    {...register("password",{
+                        required: true,
+                        minLength: 8,
+                        maxLength: 255,
+                    })}
+                />
+
+                <Button type="submit" className="w-full">
+                            Create Account
+                 </Button>
+            </div>
+          </form>
+
+        </div>
+  )
+}
+
+export default Singup
--- 12_mega_project/blog/src/components/index.js
+++ 12_mega_project/blog/src/components/index.js
@@ -1,9 +1,16 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Logo from "./Logo";
 import Container from '../container/Container'
 import Logout from "./Header/LogoutBtn";
+import Button from "./Button";
+import Input from "./Input";
 
 export {
     Header,
-    Footer 
+    Footer,
+    Container,
+    Logout,
+    Button,
+    Input,
 };
